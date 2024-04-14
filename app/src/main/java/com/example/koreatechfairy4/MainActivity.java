19a49b0ca60c3128c542a66ff642764f60886a05
여경현 <121675605+yeokyunghyun@users.noreply.github.com>
2024-04-14 13:47:03 +0900

    feat : 메인 페이지 구성1(디자인)
    메인 페이지 구성1(디자인)

--- app/src/main/java/com/example/koreatechfairy4/MainActivity.java
+++ app/src/main/java/com/example/koreatechfairy4/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.koreatechfairy4;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -13,6 +16,8 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     private TextView tv_id;
+    private ConstraintLayout notify_button, schedule_button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,5 +31,23 @@ public class MainActivity extends AppCompatActivity {
 
         tv_id = findViewById(R.id.user_id);
         tv_id.setText(getIntent().getStringExtra("userId"));
+        notify_button = findViewById(R.id.notify_button);
+        schedule_button = findViewById(R.id.schedule_button);
+
+        notify_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NotifyActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        schedule_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ScheduleActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
