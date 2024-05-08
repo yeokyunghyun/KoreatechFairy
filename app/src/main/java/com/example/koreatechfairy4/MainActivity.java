5c49a4d76bc63c207f932d6bbac2779ce12f8f6e
여경현 <121675605+yeokyunghyun@users.noreply.github.com>
2024-05-08 17:09:51 +0900

    feat : 내 정보, 성적정보 입력완료
    내 정보, 성적정보 입력완료

--- app/src/main/java/com/example/koreatechfairy4/MainActivity.java
+++ app/src/main/java/com/example/koreatechfairy4/MainActivity.java
@@ -48,20 +48,6 @@ public class MainActivity extends AppCompatActivity {
         notify_button = findViewById(R.id.notify_button);
         schedule_button = findViewById(R.id.schedule_button);
 
-        notify_title = findViewById(R.id.notification_title);
-        notify_message = findViewById(R.id.notification_message);
-        notify_btn = findViewById(R.id.notification_btn);
-        notificationHelper = new NotificationHelper(this);
-        notify_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String title = notify_title.getText().toString();
-                String msg = notify_message.getText().toString();
-                sendOnChannel(title, msg);
-            }
-        });
-        // 알림 기능
-
         userId = getIntent().getStringExtra("userId");
         HashMap<String, String> myPageMap = new HashMap<>();
         myPageMap.put("userId", userId);
@@ -88,11 +74,6 @@ public class MainActivity extends AppCompatActivity {
         });
         switchActivityWithExtra(my_page_button, MyPageActivity.class, myPageMap);
 
-    }
-
-    public void sendOnChannel(String title, String msg) {
-        NotificationCompat.Builder nb = notificationHelper.getChannel1Notification(title, msg);
-        notificationHelper.getManager().notify(1, nb.build());
     }
     @Override
     protected void onResume() {
