af681098dd910fe32ab6e0dd82c4fa13ae3da446
여경현 <121675605+yeokyunghyun@users.noreply.github.com>
2024-05-15 14:26:30 +0900

    feat : FoodActivity 생성
    FoodActivity 생성

--- app/src/main/java/com/example/koreatechfairy4/service/MyService.java
+++ app/src/main/java/com/example/koreatechfairy4/service/MyService.java
@@ -205,7 +205,7 @@ public class MyService extends Service {
         intent.putExtra("imgUrls", notify.getImgUrls());
         intent.putExtra("baseUrl", notify.getBaseUrl());
         intent.putExtra("author", notify.getAuthor());
-        PendingIntent pdIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent pdIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT | FLAG_IMMUTABLE);
         String groupKey = "groupKey_" + System.currentTimeMillis();
 
         NotificationCompat.Builder nb = notificationHelper.getChannel1Notification(title, msg);
